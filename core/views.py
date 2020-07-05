8f63d5bdad79438de2605d377732daabf52a5373
c0d33py <rootkill007@gmail.com>
2020-07-05 06:23:03 +0500

    form and views comments

--- core/views.py
+++ core/views.py
@@ -17,3 +17,33 @@ def detaillistViews(request, pk):
         'objects': post
     }
     return render(request, 'core/detail.html', context)
+
+
+# def PostCreate(request):
+# 	if request.method =='POST':
+# 		form = PostForm(request.POST)
+# 		if form.is_valid():
+# 			form.save()
+# 			message.success(request, 'your message')
+# 			return redirect('url')
+# 	else:
+# 	form : PostForm()
+# 	context = {'form': form}
+# 	return render(request, 'html', context)
+
+
+# def PostUpdate(request, id):
+#     post = get_object_or_404(Post, id=id)
+#     form = PostForm(request.POST or None, instence=post)
+#         if form.is_valid():
+#            form.save()
+#            return redirect('/article/list/')
+#     return render(request, 'core/post_detail.html', {'form':form})
+
+
+# def PostDelete(request, id):
+#     post = get_object_or_404(Post, id=id)
+#     if request.method =='POST':
+#         post.delete()
+#         return redirect('/post/list/')
+#     return render(request, 'core/post_detail.html', {'post':post})
