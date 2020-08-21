984246a59c781cd73a5bd20663eaeda93035688d
c0d33py <rootkill007@gmail.com>
2020-08-21 15:22:21 +0500

    profile model updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views as user_views
 
 
 urlpatterns = [
+    path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.Register, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import PasswordChangeForm
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, UserProfileUpdate
 
 
 def Register(request):
@@ -15,3 +14,25 @@ def Register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'user/register.html', {'form': form})
+    
+    
+    @login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = UserProfileUpdate(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your Profile has been Updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = UserProfileUpdate(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'user/profile.html', context)
